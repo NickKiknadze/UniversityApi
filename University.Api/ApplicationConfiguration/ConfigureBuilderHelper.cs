d872aeaa770f3f3055a17c73e9ee40639386d8c6
Nika Kiknadze <kiknadzenika88@gmail.com>
2025-11-15 12:38:21 +0400

    Upgrade to .NET 10 and update NuGet packages
    Updated all projects to target .NET 10.0 and upgraded related NuGet package versions, including Microsoft.AspNetCore.Authentication.JwtBearer, EntityFrameworkCore, and others. Added several new dependencies to University.Persistence for enhanced identity and data support. Also removed unused using directives in ConfigureBuilderHelper.cs.

--- University.Api/ApplicationConfiguration/ConfigureBuilderHelper.cs
+++ University.Api/ApplicationConfiguration/ConfigureBuilderHelper.cs
@@ -4,8 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using University.Application;
-using University.Application.Services;
-using University.Data;
 using University.Data.Data;
 
 namespace University.Api.ApplicationConfiguration;
